45cc278ebacf924e3c1bbceaf6bf4f803669942b
mohfirdaus22 <thepashter6@gmail.com>
2023-12-05 22:35:53 +0700

    Add class HomeViewModel private val

--- app/src/main/java/com/example/praktikum10/Model/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum10/Model/HomeViewModel.kt
@@ -4,5 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.praktikum10.Repositori.RepositoriSiswa
 
 class HomeViewModel (private val repositoriSiswa: RepositoriSiswa):ViewModel(){
-
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
