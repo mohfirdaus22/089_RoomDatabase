01b2ecd1a46390173edd03bf16805c37d603ab94
mohfirdaus22 <thepashter6@gmail.com>
2023-12-06 21:41:43 +0700

    add Add Scaffold homescreen

--- app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanHome.kt
@@ -1,14 +1,18 @@
 package com.example.praktikum10.ui.theme.Halaman
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum10.Model.HomeViewModel
 import com.example.praktikum10.Model.PenyediaViewModel
 import com.example.praktikum10.R
 import com.example.praktikum10.navigasi.DestinasiNavigasi
+import com.example.praktikum10.navigasi.SiswaTopAppBar
 
 object DestinasiHome: DestinasiNavigasi {
     override val route = "home"
@@ -24,4 +28,16 @@ fun HomeScreen(
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+    ){
+
+    }
 }
