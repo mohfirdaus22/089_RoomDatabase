6fb2289a534873f53b059874d50d3069c02c095e
mohfirdaus22 <thepashter6@gmail.com>
2023-12-06 21:04:23 +0700

    Add object DestinasiEntry

--- app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanEntry.kt
@@ -1,2 +1,9 @@
 package com.example.praktikum10.ui.theme.Halaman
 
+import com.example.praktikum10.R
+import com.example.praktikum10.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
