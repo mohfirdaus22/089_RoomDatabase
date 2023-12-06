9537cf8e4843f75fdf006c600e34ac94343b9e21
mohfirdaus22 <thepashter6@gmail.com>
2023-12-06 22:07:10 +0700

    Add CenterAligntopbar

--- app/src/main/java/com/example/praktikum10/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikum10/navigasi/PengelolaHalaman.kt
@@ -1,9 +1,12 @@
 package com.example.praktikum10.navigasi
 
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -14,5 +17,14 @@ fun SiswaTopAppBar(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
 ){
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
 
+            }
+        }
+    )
 }
