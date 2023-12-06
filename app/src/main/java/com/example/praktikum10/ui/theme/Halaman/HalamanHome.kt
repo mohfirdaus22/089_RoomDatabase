3d34f4044bf0757509ff5cc0312d75fe91a19475
mohfirdaus22 <thepashter6@gmail.com>
2023-12-06 22:01:36 +0700

    Add Text nama and telpon

--- app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanHome.kt
@@ -3,12 +3,14 @@ package com.example.praktikum10.ui.theme.Halaman
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -139,7 +141,19 @@ fun DataSiswa(
             Row (
                 modifier = Modifier.fillMaxWidth()
             ){
-
+                Text(
+                    text = siswa.nama,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null
+                )
+                Text(
+                    text = siswa.telpon,
+                    style = MaterialTheme.typography.titleMedium
+                )
             }
         }
     }
