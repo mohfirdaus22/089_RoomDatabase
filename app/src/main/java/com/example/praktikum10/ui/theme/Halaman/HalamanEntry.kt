aecbedfd7289f077c3668715c376adf1f09206ad
mohfirdaus22 <thepashter6@gmail.com>
2023-12-06 21:06:13 +0700

    Add fun EntrySiswaScreen

--- app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanEntry.kt
@@ -1,9 +1,24 @@
 package com.example.praktikum10.ui.theme.Halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikum10.Model.EntryViewModel
+import com.example.praktikum10.Model.PenyediaViewModel
 import com.example.praktikum10.R
 import com.example.praktikum10.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
+}
+
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
 }
