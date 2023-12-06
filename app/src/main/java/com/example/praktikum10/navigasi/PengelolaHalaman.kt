5365fb490bba1d9fe0add6dff10d0852f25793c3
mohfirdaus22 <thepashter6@gmail.com>
2023-12-06 22:08:02 +0700

    Add icon SiswaTopAppBar

--- app/src/main/java/com/example/praktikum10/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikum10/navigasi/PengelolaHalaman.kt
@@ -1,11 +1,17 @@
 package com.example.praktikum10.navigasi
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.praktikum10.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,7 +29,12 @@ fun SiswaTopAppBar(
         scrollBehavior = scrollBehavior,
         navigationIcon = {
             if (canNavigateBack){
-
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(id = R.string.back)
+                    )
+                }
             }
         }
     )
