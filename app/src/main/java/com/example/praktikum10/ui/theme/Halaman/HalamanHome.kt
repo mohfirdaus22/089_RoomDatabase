3c7be51960fdaf8f057f2f3b513592382971d867
mohfirdaus22 <thepashter6@gmail.com>
2023-12-06 21:45:49 +0700

    Add column bodyhome

--- app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanHome.kt
@@ -1,5 +1,6 @@
 package com.example.praktikum10.ui.theme.Halaman
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -13,6 +14,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
@@ -76,4 +78,9 @@ fun HomeScreen(
 fun BodyHome(
     itemSiswa : List<Siswa>,
     modifier: Modifier = Modifier
-) {}
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {}
+}
