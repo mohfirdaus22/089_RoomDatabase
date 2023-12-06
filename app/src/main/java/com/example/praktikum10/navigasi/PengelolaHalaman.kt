80a00f6f49a2c76a9f6940961ffbb1bc1910dc13
mohfirdaus22 <thepashter6@gmail.com>
2023-12-06 22:29:59 +0700

    Add fun SiswaApp

--- app/src/main/java/com/example/praktikum10/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikum10/navigasi/PengelolaHalaman.kt
@@ -14,13 +14,17 @@ import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.praktikum10.R
 import com.example.praktikum10.ui.theme.Halaman.DestinasiEntry
 import com.example.praktikum10.ui.theme.Halaman.DestinasiHome
 import com.example.praktikum10.ui.theme.Halaman.EntrySiswaScreen
 import com.example.praktikum10.ui.theme.Halaman.HomeScreen
 
-
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaTopAppBar(
