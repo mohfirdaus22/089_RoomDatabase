9118edc371ab22ffa37332a9e755d6db79484865
mohfirdaus22 <thepashter6@gmail.com>
2023-12-06 21:32:40 +0700

    add divider

--- app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/praktikum10/ui/theme/Halaman/HalamanEntry.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -141,5 +142,9 @@ fun FormInputSiswa(
                 modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
             )
         }
+        Divider(
+            thickness = dimensionResource(id = R.dimen.padding_small),
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium))
+        )
     }
 }
